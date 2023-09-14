a4f98c1243c2a4270ba103e7d9a486c3dcde0a27
Yavuz Erkal <erkalkeremyavuz@gmail.com>
2023-09-14 09:53:29 +0200

    export 2

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.get('/download-tcpdump-files', (req, res) => {
 
         // Append all txt files to the archive
         txtFiles.forEach(file => {
-            const filePath = path.join(__dirname, 'export', file);
+            const filePath = path.join(__dirname, 'tcpdump_logs', file);
             archive.append(fs.createReadStream(filePath), { name: file });
         });
 
